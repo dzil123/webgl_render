af5ff1cc52b297d4061f2c5388c6baf93e9ffd4d
dzil123 <5725958+dzil123@users.noreply.github.com>
2023-07-08 05:08:24 -0700

    fix generate_polygon

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -108,8 +108,8 @@ while (true) {
 
 function generate_polygon(count: number): [Float32Array, Uint8Array] {
   let delta_angle = (Math.PI * 2) / count;
-  let start_angle = delta_angle * -0.5;
-  let scale = 1.0 / Math.cos(start_angle);
+  let start_angle = (Math.PI + delta_angle) * -0.5;
+  let scale = 1.0 / Math.cos(delta_angle * 0.5);
 
   let array = new Float32Array(count * 2);
 
@@ -117,8 +117,8 @@ function generate_polygon(count: number): [Float32Array, Uint8Array] {
     let angle = start_angle + delta_angle * i;
     console.log((angle * 180) / Math.PI);
 
-    array[i * 2] = Math.sin(angle) * scale;
-    array[i * 2 + 1] = -Math.cos(angle) * scale;
+    array[i * 2] = Math.cos(angle) * scale;
+    array[i * 2 + 1] = Math.sin(angle) * scale;
   }
 
   console.log(array);
