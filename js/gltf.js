f2d68483bf8876ddc73482e8aae771714950112a
dzil123 <5725958+dzil123@users.noreply.github.com>
2023-07-09 14:08:36 +0000

    Deploying to gh-pages from @ dzil123/webgl_render@f470412a0e4d00c9daf77b09664b11a5deb77527 🚀

--- js/gltf.js
+++ js/gltf.js
@@ -6,6 +6,28 @@ const GLTF_DEFAULT = {
     accessors: [],
     bufferViews: [],
 };
+var AccessorTypeEnum;
+(function (AccessorTypeEnum) {
+    AccessorTypeEnum[AccessorTypeEnum["SCALAR"] = 0] = "SCALAR";
+    AccessorTypeEnum[AccessorTypeEnum["VEC2"] = 1] = "VEC2";
+    AccessorTypeEnum[AccessorTypeEnum["VEC3"] = 2] = "VEC3";
+    AccessorTypeEnum[AccessorTypeEnum["VEC4"] = 3] = "VEC4";
+    AccessorTypeEnum[AccessorTypeEnum["MAT2"] = 4] = "MAT2";
+    AccessorTypeEnum[AccessorTypeEnum["MAT3"] = 5] = "MAT3";
+    AccessorTypeEnum[AccessorTypeEnum["MAT4"] = 6] = "MAT4";
+})(AccessorTypeEnum || (AccessorTypeEnum = {}));
+function accessorTypeToNumComponents(t) {
+    const LUT = {
+        [AccessorTypeEnum.SCALAR]: 1,
+        [AccessorTypeEnum.VEC2]: 2,
+        [AccessorTypeEnum.VEC3]: 3,
+        [AccessorTypeEnum.VEC4]: 4,
+        [AccessorTypeEnum.MAT2]: 4,
+        [AccessorTypeEnum.MAT3]: 9,
+        [AccessorTypeEnum.MAT4]: 16,
+    };
+    return LUT[AccessorTypeEnum[t]];
+}
 async function downloadBuffer(buffer) {
     let uri = util.nonnull(buffer.uri);
     let data = await util.download("models/", uri, (r) => r.arrayBuffer());
@@ -14,11 +36,11 @@ async function downloadBuffer(buffer) {
     }
     return data;
 }
-function loadBufferView(gl, bufferView, buffers) {
+function loadBufferView(gl, bufferView, scene) {
     if (bufferView.byteStride !== undefined) {
         throw "byteStride unsupported";
     }
-    let array = util.nonnull(buffers[bufferView.buffer]);
+    let array = util.nonnull(scene.buffers[bufferView.buffer]);
     let offset = bufferView.byteOffset || 0;
     let target = util.nonnull(bufferView.target);
     // TODO: is it ok to use Uint8 regardless of AccessorType?
@@ -32,15 +54,21 @@ async function downloadGltf(name) {
     let gltf = await util.download("models/", name, (r) => r.json());
     console.dir(gltf);
     if (gltf?.asset?.version === "2.0") {
-        return Object.assign(GLTF_DEFAULT, gltf);
+        return { ...GLTF_DEFAULT, ...gltf };
     }
     throw { msg: "Unsupported gltf", asset: gltf.asset };
 }
 export async function loadGltf(gl, name) {
     let gltf = await downloadGltf(name);
-    let buffers = await Promise.all(gltf.buffers.map(downloadBuffer));
-    let bufferViews = gltf.bufferViews.map((v) => loadBufferView(gl, v, buffers));
-    let scene = { buffers, bufferViews };
+    const [buildBuffers, buildBufferViews] = [
+        async () => ({ buffers: await Promise.all(gltf.buffers.map(downloadBuffer)) }),
+        (scene) => ({
+            ...scene,
+            bufferViews: gltf.bufferViews.map((v) => loadBufferView(gl, v, scene)),
+        }),
+    ];
+    let scene1 = await buildBuffers();
+    let scene = buildBufferViews(scene1);
     return [gltf, scene];
 }
 //# sourceMappingURL=gltf.js.map
